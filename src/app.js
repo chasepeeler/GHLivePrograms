d24912d25907736a15098995789fdec33582e076
Chase Peeler <chasepeeler@gmail.com>
2016-01-20 17:25:29 -0500

    Initial implementation with subscriptions module

--- src/app.js
+++ src/app.js
@@ -1,62 +1,29 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+/* Required libraries */
+var Subscription = require("subscriptions");
 
-var UI = require('ui');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
-});
+var s = new Subscription({title: "GHTV",subtitle:"Programming"});
 
-main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+//topics
+var topics = [
+  {id: "anthems", title:"Anthems", subscribed: false, icon:""},
+  {id: "blockbusters", title:"Blockbusters", subscribed: false, icon:""},
+  {id: "classics", title:"Classics", subscribed: false, icon:""},
+  {id: "headliners", title:"Headliners", subscribed: false, icon:""},
+  {id: "hits", title:"Hits", subscribed: false, icon:""},
+  {id: "indie", title:"Indie", subscribed: false, icon:""},
+  {id: "jams", title:"Jams", subscribed: false, icon:""},
+  {id: "knockouts", title:"Knockouts", subscribed: false, icon:""},
+  {id: "metal", title:"Metal", subscribed: false, icon:""},
+  {id: "picks", title:"Picks", subscribed: false, icon:""},
+  {id: "pop", title:"Pop", subscribed: false, icon:""},
+  {id: "riffs", title:"Riffs", subscribed: false, icon:""},
+  {id: "rock", title:"Rock", subscribed: false, icon:""},
+  {id: "smashes", title:"Smashes", subscribed: false, icon:""},
+  {id: "other", title:"Other", subscribed: false, icon:""}
+];
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+s.addTopics(topics);
+s.start();
